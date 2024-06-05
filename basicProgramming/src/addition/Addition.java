57cb5c36744cc3c1a1918565e00b59cdce7f7fe7
nikhilpurwant8791 <purwantn@gmail.com>
2024-06-05 22:12:25 +0530

    basic-02| Commit Using IntelliJ

--- basicProgramming/src/addition/Addition.java
+++ basicProgramming/src/addition/Addition.java
@@ -4,13 +4,14 @@ public class Addition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a =2;
-		int b= 3;
+		float a =2;
+		float b= 3;
 		int c = a+b;
 		int d = b-a;
 		System.out.println("Addition of a and b is "+c);
 		System.out.println("Substraction of a from b is "+d);
-		System.out.println("Multiplication of a from b is "+ (a*b));
+		System.out.println("Multiplication of a and b is "+ (a*b));
+		System.out.println("Division of a and b is "+ (a/b));
 		
 	}
 
