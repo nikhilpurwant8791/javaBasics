4a374d03a5d5ac734a10959ed5359b058f9c6ec3
nikhilpurwant8791 <purwantn@gmail.com>
2024-06-07 19:40:46 +0530

    basic-01| Special Character

---
+++ basicProgramming/src/common/SpecialCharacter.java
@@ -0,0 +1,22 @@
+package common;
+
+public class SpecialCharacter {
+	public static void special() {
+		String b ="< > , . / ? ; : [ ] { } | ! @ # $ % ^ & * ( ) ";
+		String a = "My Name is m@r # $ ^ % & & * *" ;
+		//		String[] sp = s.split(" ");
+		char[] c = b.toCharArray();
+		char[] d = a.toCharArray();
+		for (int i = 0; i < d.length-1; i++) {
+			for (int j=0; j<=c.length-1; j++) {
+				if(d[i] == c[j] && d[i]!= ' ') {
+					System.out.println("special Character is "+ c[j]);
+				}
+			}
+		}
+	}
+
+	public static void main (String[] args) {
+		special();
+	}
+}
